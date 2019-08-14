e433cf3400123109e4a31216124e4a03c555ef25
Juan David Rios <juandarr@gmail.com>
2019-08-14 15:37:37 -0500

    feat: completed first version of data read class; add: overloads __str__ method and define load image method; doc: variables and methods commented to improve readability.

--- MediaPlayer/model.py
+++ MediaPlayer/model.py
@@ -1,3 +1,5 @@
+import csv 
+
 class data_reader:
     """
     Reads csv file inside 'path' and stores image locations in a 3-dimensional list whose indexes are defined as follows:
@@ -16,6 +18,31 @@ class data_reader:
         self.camera_labels = {} # Camera labels definition
         
         self.images = [[[]]] # 3-dimensional tensor storing image paths according to capture index, camera index and timestamp index  
+        
+        self.line_count = None # Used to keep track of read rows from the csv file
+        self.header_format = None # used to keep track of header rwo from csv file
+
+    def __str__(self):
+        """
+        Overloads __str__ method with custom object attributes with information about:
+            - Camera labels - which are the same for a given set of captures stored in a csv file
+            - Captures
+                - Number of cameras per capture
+                - Number of timestamps per capture
+        """
+        message = '----DATA READER SUMMARY----\n\n'
+        message += 'Camera labels:\n'
+        message += self.camera_labels.__str__() + '\n'
+        
+        index = 0   
+        for capture in self.images:
+            message += '\nCapture {}\n'.format(index)
+            message += '{} Cameras\n'.format(len(capture))
+            message += '{} timestamps in total\n'.format(len(capture[0]))
+            index += 1
+        message += self.header_format
+        message += '\n----Total lines read from the csv file: {} ----'.format(self.line_count)
+        return message
 
     def load_data(self):
         """
@@ -26,7 +53,7 @@ class data_reader:
         # Open csv file using the absolute path
         with open(self.path + '/data.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
+            self.line_count = 0
 
             current_capture = None # Current capture index
             current_timestamp = None # Current timestamp value
@@ -36,65 +63,63 @@ class data_reader:
             # Read csv file one row at a time
             for row in csv_reader:
                 # If line is header line print the column names
-                if line_count == 0:
-                    print('Column names are {}'.format(", ".join(row)))
-                    line_count += 1
+                if self.line_count == 0:
+                    self.header_format = '\n----Column names are: {}----'.format(" | ".join(row))
+                    self.line_count += 1
                 # Else read each new row and store values in the 3-d array self.images
                 else:
                     
-                    # If current_capture has not been defined, define it
+                    # If current_capture has not been defined, define it with current row capture value
                     if current_capture == None:
                         current_capture = int(row[0])
+                    # When capture value changes
                     elif current_capture != int(row[0]):
-                        current_capture = int(row[0])
-                        self.images.append([[]])
-                        self.timestamps.append([])
-                        is_new_capture =  True
-                        current_timestamp = None
-                        print('There is a new capture!: {}'.format(current_capture))
+                        current_capture = int(row[0]) # Stores current capture in row
+                        self.images.append([[]]) # Adds a new 2-d list to include information of new capture
+                        self.timestamps.append([]) # Adds new list to include timestamp sequence of new capture
+                        is_new_capture =  True # Set flag to true, since a new capture is being stored
+                        current_timestamp = None # Reset current timestamp
                     
+                    # If current timestamp has not been defined
                     if current_timestamp == None:
-                        current_timestamp = row[1]
-                        timestamps[current_capture].append(current_timestamp)
-                        camera_index = 0
+                        current_timestamp = row[1] # Define timestamp of current row
+                        self.timestamps[current_capture].append(current_timestamp) # Append timestamp to the list of timestamps of current capture
+                        camera_index = 0 # Init camera index as 0
+                    # When current timestamp changes
                     elif current_timestamp != row[1]:
-                        if not(camera_labels_stored):
-                            camera_labels_stored = True
-                        if new_capture:
-                            new_capture = False
-                        current_timestamp = row[1]
-                        timestamps[current_capture].append(current_timestamp)
-                        camera_index = 0
+                        # Set are_camera_labels_stores flag to true
+                        if not(are_camera_labels_stored):
+                            are_camera_labels_stored = True
+                        # Set is_new_capture to false 
+                        if is_new_capture:
+                            is_new_capture = False
+                        current_timestamp = row[1] # Define timestamp of current row
+                        self.timestamps[current_capture].append(current_timestamp) # Append timestamp to the list of timestamps of current capture
+                        camera_index = 0 # Init camera index as 0
                     else:
-                        if new_capture:
-                            images[current_capture].append([])
-                            print('-----------This just happened!-------------')
+                        # If is_new_capture extend dimensonality of current image capture 2-d list
+                        if is_new_capture:
+                            self.images[current_capture].append([])
                     
-                    if not(camera_labels_stored):
-                        if row[2] not in camera_labels: 
-                            camera_labels[row[2]] = camera_index
-
-                    print(current_capture, camera_index, row[3])
-
-                    images[current_capture][camera_index].append(row[3])
+                    # If camera labels have not been store, store them in camera_labels
+                    if not(are_camera_labels_stored):
+                        if row[2] not in self.camera_labels: 
+                            self.camera_labels[row[2]] = camera_index
                     
+                    # Store image path in current_capture, camera_index and timestamp index positions
+                    self.images[current_capture][camera_index].append(row[3])
+                    
+                    # Increase camera index
                     camera_index += 1
-
-                    print('Picture taken in capture {0} with timestamp {1} and camera {2}.\nLocation is: {3}'.format(row[0],row[1],row[2],row[3]))
-                    line_count += 1
-            print(camera_labels)
-            print('\nSummary of storage\n')
-            index = 0
-            for capture in images:
-                print('Capture {}\n'.format(index))
-                print('{} Cameras\n'.format(len(capture)))
-                print('{} timestamps in total\n'.format(len(capture[0])))
-                index += 1
-            print('Processed {} lines.'.format(line_count))
+                    self.line_count += 1
     
     def get_image(self, timestamp_idx, camera_idx, capture_idx):
         """
         Get an image given a timestamp index, camera type and capture index
         """
-        pass
+        return self.images[capture_idx][camera_idx][timestamp_idx]
 
+if __name__ == '__main__':
+    reader = data_reader('/media/overcode/eagle/data_capture-08-08-19')
+    reader.load_data()
+    print(reader)
