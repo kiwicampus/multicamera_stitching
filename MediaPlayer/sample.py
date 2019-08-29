c27399697c20dc1a678e8e454d4e19ac288dd513
Juan David Rios <juandarr@gmail.com>
2019-08-29 17:47:29 -0500

    feat: add multiple files for tests and iterations used during the development. UI implementation and code revision will be next.

---
+++ MediaPlayer/sample.py
@@ -0,0 +1,65 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QPen
+from PyQt5.QtCore import Qt
+import cv2
+
+class ImageVisor(QWidget):
+    def __init__(self, img_path, parent=None):
+        QWidget.__init__(self, parent=parent)
+        self.index = 0
+        self.image_path = img_path
+        self.pixmap = QPixmap(self.image_path[self.index])
+        self.pos = None
+        self.pos_click = None
+        self.setMouseTracking(True)
+        self.marks = []
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), self.pixmap)
+        if self.pos:
+            painter.setPen(QPen(Qt.blue, 5, Qt.SolidLine))
+            painter.drawEllipse(self.pos, 5, 5)
+        if self.pos_click:
+            for pos in self.marks:
+                painter.setPen(QPen(Qt.red, 10, Qt.SolidLine))
+                painter.drawEllipse(pos, 5, 5)
+
+    def mouseMoveEvent(self, event):
+        self.pos = event.pos()
+        print('New position!: ',self.pos)
+        #self.update()
+
+    def mousePressEvent(self, event):
+        self.pos_click = event.pos()
+        print('New click on image!', self.pos_click)
+        self.marks.append(self.pos_click)
+
+        self.index += 1
+        if self.index == len(self.image_path):
+            self.index = 0
+        self.setImage(self.image_path[self.index])
+        self.update()
+
+    def setImage(self, path):
+        pixmap = QPixmap(path)
+        self.pixmap = pixmap
+        
+class ImageWindow(QWidget):
+    def __init__(self, image_path, parent=None):
+        QWidget.__init__(self, parent=parent)
+        self.setLayout(QVBoxLayout())
+        img = cv2.imread(image_path[0])
+        print('Size: ', img.shape)
+        
+        self.label = ImageVisor(image_path)
+        self.layout().addWidget(self.label)
+        self.resize(img.shape[1]//2,img.shape[0]//2)
+        #self.resize(640,360)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = ImageWindow(['img1.jpg' , 'img2.jpg' , 'img3.jpg'])
+    w.show()
+    sys.exit(app.exec_())   
--- MediaPlayer/ui design pyqt/ui_loader_icon.py
+++ MediaPlayer/ui design pyqt/ui_loader_icon.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from decimal import Decimal
 
-qtcreator_file  = "ui/icon_test.ui" # Enter file here.
+qtcreator_file  = "ui/icon_test_v2.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtcreator_file)
 
 
@@ -11,47 +11,48 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-
+        self.i = 35
         self.image_view.ui.histogram.hide() # Hides histogram from image frame
         self.image_view.ui.roiBtn.hide() # Hides roi button from image frame
         self.image_view.ui.menuBtn.hide() # Hides menu button from image frame
         
-        self.stitcher_view.ui.histogram.hide() # Hides histogram from image frame
-        self.stitcher_view.ui.roiBtn.hide() # Hides roi button from image frame
-        self.stitcher_view.ui.menuBtn.hide() # Hides menu button from image frame
+        #self.stitcher_view.ui.histogram.hide() # Hides histogram from image frame
+        #self.stitcher_view.ui.roiBtn.hide() # Hides roi button from image frame
+        #self.stitcher_view.ui.menuBtn.hide() # Hides menu button from image frame
 
         rMyIcon = QtGui.QPixmap("icons/usb.png");
         self.button_loadfile.setIcon(QtGui.QIcon(rMyIcon))
-        self.button_loadfile.setIconSize(QtCore.QSize(48,48))
+        self.button_loadfile.setIconSize(QtCore.QSize(self.i,self.i))
 
         prevMyIcon = QtGui.QPixmap("icons/previous-1.png");
         self.button_previous_capture.setIcon(QtGui.QIcon(prevMyIcon))
-        self.button_previous_capture.setIconSize(QtCore.QSize(48,48))
+        self.button_previous_capture.setIconSize(QtCore.QSize(self.i,self.i))
 
         self.button_previous_camera.setIcon(QtGui.QIcon(prevMyIcon))
-        self.button_previous_camera.setIconSize(QtCore.QSize(48,48))
+        self.button_previous_camera.setIconSize(QtCore.QSize(self.i,self.i))
 
         nextMyIcon = QtGui.QPixmap("icons/skip-1.png");
         self.button_next_capture.setIcon(QtGui.QIcon(nextMyIcon))
-        self.button_next_capture.setIconSize(QtCore.QSize(48,48))
+        self.button_next_capture.setIconSize(QtCore.QSize(self.i,self.i))
 
         self.button_next_camera.setIcon(QtGui.QIcon(nextMyIcon))
-        self.button_next_camera.setIconSize(QtCore.QSize(48,48))
+        self.button_next_camera.setIconSize(QtCore.QSize(self.i,self.i))
         
         
         playMyIcon = QtGui.QPixmap("icons/play.png");
         pauseMyIcon = QtGui.QPixmap("icons/pause.png");
         self.play_pause_icons = [QtGui.QPixmap("icons/pause.png") , QtGui.QPixmap("icons/play.png")]
         self.button_play.setIcon(QtGui.QIcon(playMyIcon))
-        self.button_play.setIconSize(QtCore.QSize(48,48))
+        self.button_play.setIconSize(QtCore.QSize(self.i,self.i))
 
         self.button_play.clicked.connect(self.toggle_icons)
         self.play = True
+        print(dir(self.stitcher_view))
 
     def toggle_icons(self):
         self.play = not(self.play)
         self.button_play.setIcon(QtGui.QIcon(self.play_pause_icons[int(self.play)]))
-        self.button_play.setIconSize(QtCore.QSize(48,48))
+        self.button_play.setIconSize(QtCore.QSize(self.i,self.i))
 
 
 if __name__ == "__main__":
